96d38a917a8cae0a67505e605cb62d3e9bf6f0bd
AdamKorcz <adam@adalogics.com>
2022-01-24 12:20:42 +0000

    fuzzing: Rewrite fuzzers to Go 1.18 harnesses

---
+++ tests/fuzzers/runtime/native_runtime_fuzz_test.go
@@ -0,0 +1,33 @@
+// Copyright 2022 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package runtime
+
+import (
+	"github.com/ethereum/go-ethereum/core/vm/runtime"
+)
+
+// Fuzz is the basic entry point for the go-fuzz tool
+//
+// This returns 1 for valid parsable/runable code, 0
+// for invalid opcode.
+func FuzzRuntimeNative(f *testing.F) {
+	f.Fuzz(func(t *testing.T, input1, input2 string) {
+		_, _, err := runtime.Execute(input1, input2, &runtime.Config{
+			GasLimit: 12000000,
+		})
+	})
+}
